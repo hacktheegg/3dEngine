3063470e87c4fa7e01da3b435de1607e677a4e7a
hacktheegg <>
2024-02-11 21:55:07 +1100

    initial commit

---
+++ 3dEngine.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main() {
+    // Initialize the library
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // Create a windowed mode window and its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(640,  480, "Hello World", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Loop until the user closes the window
+    while (!glfwWindowShouldClose(window)) {
+        // Render here
+        glClearColor(0.2f,  0.3f,  0.3f,  1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return  0;
+}
