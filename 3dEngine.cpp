03a232851f260f90fcdbb8d0a045757e11982e98
hacktheegg <>
2024-02-16 20:13:37 +1100

    Compiles in linux

--- 3dEngine.cpp
+++ 3dEngine.cpp
@@ -1,5 +1,24 @@
+#include <GL/gl.h>
 #include <iostream>
 #include <GLFW/glfw3.h>
+
+
+
+void drawSquare(double x1, double y1, double sideLength) {
+  double halfSide = sideLength;
+
+  glColor3d(0, 0, 0);
+  glBegin(GL_POLYGON);
+
+  glVertex2d(x1 + halfSide, y1 + halfSide);
+  glVertex2d(x1 + halfSide, y1 - halfSide);
+  glVertex2d(x1 - halfSide, y1 - halfSide);
+  glVertex2d(x1 - halfSide, y1 + halfSide);
+
+  glEnd();
+}
+
+
 
 int main() {
     // Initialize the library
@@ -24,6 +43,8 @@ int main() {
         // Render here
         glClearColor(0.2f,  0.3f,  0.3f,  1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        drawSquare(0.0, 0.0, 0.2);
 
         // Swap front and back buffers
         glfwSwapBuffers(window);
