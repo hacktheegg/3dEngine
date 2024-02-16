a7a2e306bb65c150ed3f096fffb664549dec816a
hacktheegg <>
2024-02-17 01:09:13 +1100

    started progress on rendering in 3d, following [this](https://www.youtube.com/watch?v=ih20l3pJoeU) tutorial

--- 3dEngine.cpp
+++ 3dEngine.cpp
@@ -1,6 +1,7 @@
 #include <GL/gl.h>
 #include <iostream>
 #include <GLFW/glfw3.h>
+#include <vector>
 
 
 
@@ -21,38 +22,40 @@ void drawSquare(double x1, double y1, double sideLength) {
 
 
 int main() {
-    // Initialize the library
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
-        return -1;
-    }
+  // Initialize the library
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW" << std::endl;
+    return -1;
+  }
 
-    // Create a windowed mode window and its OpenGL context
-    GLFWwindow* window = glfwCreateWindow(640,  480, "Hello World", NULL, NULL);
-    if (!window) {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-
-    // Make the window's context current
-    glfwMakeContextCurrent(window);
-
-    // Loop until the user closes the window
-    while (!glfwWindowShouldClose(window)) {
-        // Render here
-        glClearColor(0.2f,  0.3f,  0.3f,  1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        drawSquare(0.0, 0.0, 0.2);
-
-        // Swap front and back buffers
-        glfwSwapBuffers(window);
-
-        // Poll for and process events
-        glfwPollEvents();
-    }
-
+  // Create a windowed mode window and its OpenGL context
+  GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  if (!window) {
+    std::cerr << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
-    return  0;
+    return -1;
+  }
+
+  // Make the window's context current
+  glfwMakeContextCurrent(window);
+
+  std::vector<std::vector<int>> square { {10, 10}, {10, -10}, {-10, -10}, {-10, 10} };
+  
+  // Loop until the user closes the window
+  while (!glfwWindowShouldClose(window)) {
+    // Render here
+    glClearColor(0.2f,  0.3f,  0.3f,  1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    drawSquare(0.0, 0.0, 0.2);
+
+    // Swap front and back buffers
+    glfwSwapBuffers(window);
+
+    // Poll for and process events
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return  0;
 }
