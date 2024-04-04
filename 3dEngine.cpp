67f334e406a346542c762e193f9d89266d5197a2
hacktheegg <>
2024-04-04 23:40:16 +1100

    moved the main loop into the engine3d class so it can actually utilise the variables and functions in there

--- 3dEngine.cpp
+++ 3dEngine.cpp
@@ -24,23 +24,75 @@ struct matrix4by4 {
 
 
 
-void MultiplyMatrixVector(vector3d &i, vector3d &o, matrix4by4 &m) {
-  o.x = (i.x * m.m[0][0]) + (i.y * m.m[1][0]) + (i.z * m.m[2][0]) + m.m[3][0];
-  o.y = (i.x * m.m[0][1]) + (i.y * m.m[1][1]) + (i.z * m.m[2][1]) + m.m[3][1];
-  o.z = (i.x * m.m[0][2]) + (i.y * m.m[1][2]) + (i.z * m.m[2][2]) + m.m[3][2];
-  float w = (i.x * m.m[0][3]) + (i.y * m.m[1][3]) + (i.z * m.m[2][3]) + m.m[3][3];
 
-  if (w != 0.0f) {
-    o.x /= w; o.y /= w; o.z /= w;
-  }
-}
 class engine3d {
   private:
     mesh meshCube;
     matrix4by4 matrixProjection;
 
+    void MultiplyMatrixVector(vector3d &i, vector3d &o, matrix4by4 &m) {
+      o.x = (i.x * m.m[0][0]) + (i.y * m.m[1][0]) + (i.z * m.m[2][0]) + m.m[3][0];
+      o.y = (i.x * m.m[0][1]) + (i.y * m.m[1][1]) + (i.z * m.m[2][1]) + m.m[3][1];
+      o.z = (i.x * m.m[0][2]) + (i.y * m.m[1][2]) + (i.z * m.m[2][2]) + m.m[3][2];
+      float w = (i.x * m.m[0][3]) + (i.y * m.m[1][3]) + (i.z * m.m[2][3]) + m.m[3][3];
+
+      if (w != 0.0f) {
+        o.x /= w; o.y /= w; o.z /= w;
+      }
+    }
 
   public:
+    int mainLoop() {
+      // Initialize the library
+      if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+      }
+      // Create a windowed mode window and its OpenGL context
+      GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+      if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+      }
+
+
+
+      // Make the window's context current
+      glfwMakeContextCurrent(window);
+      // Loop until the user closes the window
+      while (!glfwWindowShouldClose(window)) {
+        // Render here
+        glClearColor(0.2f,  0.3f,  0.3f,  1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // drawSquare(0.0, 0.0, 0.2);
+        /*double halfSide = 0.2;
+
+        glColor3d(0, 0, 0);
+        glBegin(GL_POLYGON);
+
+        glVertex2d(0.0f + halfSide, 0.0f + halfSide);
+        glVertex2d(0.0f + halfSide, 0.0f - halfSide);
+        glVertex2d(0.0f - halfSide, 0.0f - halfSide);
+        glVertex2d(0.0f - halfSide, 0.0f + halfSide);
+
+        glEnd();*/
+
+
+
+
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+      }
+
+      glfwTerminate();
+      return 0;
+    }
     bool onCreate(GLFWwindow* window) {
 
       meshCube.tris = {
@@ -96,56 +148,9 @@ class engine3d {
 
 
 int main() {
-  // Initialize the library
-  if (!glfwInit()) {
-    std::cerr << "Failed to initialize GLFW" << std::endl;
-    return -1;
-  }
-  // Create a windowed mode window and its OpenGL context
-  GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-  if (!window) {
-    std::cerr << "Failed to create GLFW window" << std::endl;
-    glfwTerminate();
-    return -1;
-  }
-
-
-
-  // Make the window's context current
-  glfwMakeContextCurrent(window);
-  // Loop until the user closes the window
-  while (!glfwWindowShouldClose(window)) {
-    // Render here
-    glClearColor(0.2f,  0.3f,  0.3f,  1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    // drawSquare(0.0, 0.0, 0.2);
-    /*double halfSide = 0.2;
-
-    glColor3d(0, 0, 0);
-    glBegin(GL_POLYGON);
-
-    glVertex2d(0.0f + halfSide, 0.0f + halfSide);
-    glVertex2d(0.0f + halfSide, 0.0f - halfSide);
-    glVertex2d(0.0f - halfSide, 0.0f - halfSide);
-    glVertex2d(0.0f - halfSide, 0.0f + halfSide);
-
-    glEnd();*/
-
-
-
-
-
-    // Swap front and back buffers
-    glfwSwapBuffers(window);
-
-    // Poll for and process events
-    glfwPollEvents();
-  }
-
-  glfwTerminate();
-  return  0;
-
+  engine3d engine;
+  int var = engine.mainLoop();
+  return var;
 }
 
 
@@ -194,27 +199,27 @@ int main() {
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
-        return -1;
-    }
+      return -1;
+  }
 
-    // Make the window's context current
-    glfwMakeContextCurrent(window);
+  // Make the window's context current
+  glfwMakeContextCurrent(window);
 
-    // Loop until the user closes the window
-    while (!glfwWindowShouldClose(window)) {
-        // Render here
-        glClearColor(0.2f,  0.3f,  0.3f,  1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+  // Loop until the user closes the window
+  while (!glfwWindowShouldClose(window)) {
+      // Render here
+      glClearColor(0.2f,  0.3f,  0.3f,  1.0f);
+      glClear(GL_COLOR_BUFFER_BIT);
 
-        drawSquare(0.0, 0.0, 0.2);
+      drawSquare(0.0, 0.0, 0.2);
 
-        // Swap front and back buffers
-        glfwSwapBuffers(window);
+      // Swap front and back buffers
+      glfwSwapBuffers(window);
 
-        // Poll for and process events
-        glfwPollEvents();
-    }
+      // Poll for and process events
+      glfwPollEvents();
+  }
 
-    glfwTerminate();
-    return  0;
+  glfwTerminate();
+  return  0;
 }*/
